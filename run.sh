a63d82acd05b7c1113c4155414ffdd364676ced2
kelostrada <kelostrada@gmail.com>
2016-06-25 00:19:21 +0200

    final version of grammar

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#docker run -it --rm --name bnfc -v $PWD:/app kelu/bnfc bnfc -m -cpp_stl calc.cf
+docker run -it --rm --name bnfc -v $PWD:/app kelu/bnfc make
+docker run -it --rm --name bnfc -v $PWD:/app kelu/bnfc ./Testcalc testFile
